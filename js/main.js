228dc2afb0406275b3c3988485a456672a6e4a2c
FlorisMerckx <32137004+FlorisMerckx@users.noreply.github.com>
2018-12-19 18:22:49 +0100

    First online test
    Hello world!

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+
+    $("#menu").hide();
+
+    $("#menuToggle").click(function(){
+        $("#menu").toggleClass("hide-menu");
+        $("#menu").show();
+
+    });
+
+    $(".movingclouds1").animate( {top:
+        "-=" + (parseInt($(".movingclouds1").css("top")) - 430) + "px"
+      });
+    
+      $(".movingclouds2").animate( {top:
+        "-=" + (parseInt($(".movingclouds2").css("top")) - 60) + "px"
+      });
+
+      $(".movingclouds3").animate( {right:
+        "-=" + (parseInt($(".movingclouds3").css("right")) - 70) + "px"
+      });
+    
+
+      $('.checkbox').click(function(){
+        $(this).toggleClass('is-checked');
+      });
+    
+
+});
