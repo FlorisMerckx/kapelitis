bfe082045250de640f7ab527792c87768c85927a
FlorisMerckx <32137004+FlorisMerckx@users.noreply.github.com>
2018-12-21 16:55:15 +0100

    beautified the code

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-    $("#menu").hide();
+  $("#menu").hide();
 
-    $("#menuToggle").click(function(){
-        $("#menu").toggleClass("hide-menu");
-        $("#menu").show();
+  $("#menuToggle").click(function () {
+    $("#menu").toggleClass("hide-menu");
+    $("#menu").show();
 
-    });
+  });
 
-    $(".movingclouds1").animate( {top:
-        "-=" + (parseInt($(".movingclouds1").css("top")) - 430) + "px"
-      });
-    
-      $(".movingclouds2").animate( {top:
-        "-=" + (parseInt($(".movingclouds2").css("top")) - 60) + "px"
-      });
+  $(".movingclouds1").animate({
+    top: "-=" + (parseInt($(".movingclouds1").css("top")) - 430) + "px"
+  });
 
-      $(".movingclouds3").animate( {right:
-        "-=" + (parseInt($(".movingclouds3").css("right")) - 70) + "px"
-      });
-    
+  $(".movingclouds2").animate({
+    top: "-=" + (parseInt($(".movingclouds2").css("top")) - 60) + "px"
+  });
 
-      $('.checkbox').click(function(){
-        $(this).toggleClass('is-checked');
-      });
-    
+  $(".movingclouds3").animate({
+    right: "-=" + (parseInt($(".movingclouds3").css("right")) - 70) + "px"
+  });
+
+
+  $('.checkbox').click(function () {
+    $(this).toggleClass('is-checked');
+  });
+
 
 });
